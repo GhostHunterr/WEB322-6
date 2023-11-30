8566308e981798283dfe8a7c7d525528d112ea00
Farhan Sarang <farhansarang7@gmail.com>
2023-11-29 23:35:38 -0500

    First Commit

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 /********************************************************************************
-*  WEB322 – Assignment 04
-* 
-*  I declare that this assignment is my own work in accordance with Seneca's
-*  Academic Integrity Policy:
-* 
-*  https://www.senecacollege.ca/about/policies/academic-integrity-policy.html
-* 
-*  Name: Farhan Sarang Student ID: 172963217 Date: 13/11/2023
-*
-*  Published URL: ___________________________________________________________
-*
-********************************************************************************/
+ *  WEB322 – Assignment 05
+ *
+ *  I declare that this assignment is my own work in accordance with Seneca's
+ *  Academic Integrity Policy:
+ *
+ *  https://www.senecacollege.ca/about/policies/academic-integrity-policy.html
+ *
+ *  Name: Farhan Sarang Student ID: 172963217 Date: 29/11/2023
+ *
+ *  Published URL:
+ *
+ ********************************************************************************/
 
 const legoData = require("./modules/legoSets");
 const path = require("path");
@@ -71,6 +71,57 @@ app.get("/lego/sets/:num", async (req, res) => {
   } catch (err) {
     res.status(404).render("404", {
       message: "I'm sorry, we're unable to find what you're looking for",
+    });
+  }
+});
+
+app.get("/lego/addSet", async (req, res) => {
+  try {
+    const themes = await legoData.getAllThemes();
+    res.render("addSet", { themes: themes }); // Render the addSet view with themes data
+  } catch (err) {
+    res.render("500", { message: `Error: ${err}` }); // Render the 500 view if an error occurs
+  }
+});
+
+// POST route to handle form submission and add a new set
+app.post("/lego/addSet", async (req, res) => {
+  try {
+    await legoData.addSet(req.body); // Call the addSet function with form data (req.body)
+    res.redirect("/lego/sets"); // Redirect to the sets route after successful addition
+  } catch (err) {
+    res.render("500", { message: `Error: ${err}` }); // Render the 500 view if an error occurs
+  }
+});
+
+app.get("/lego/editSet/:num", async (req, res) => {
+  try {
+    const set = await legoData.getSetByNum(req.params.num);
+    const themes = await legoData.getAllThemes();
+    res.render("editSet", { set, themes });
+  } catch (err) {
+    res.status(404).render("404", { message: err });
+  }
+});
+
+app.post("/lego/editSet", async (req, res) => {
+  try {
+    await legoData.editSet(req.body.set_num, req.body);
+    res.redirect("/lego/sets");
+  } catch (err) {
+    res.render("500", {
+      message: `I'm sorry, but we have encountered the following error: ${err}`,
+    });
+  }
+});
+
+app.get("/lego/deleteSet/:num", async (req, res) => {
+  try {
+    await legoData.deleteSet(req.params.num);
+    res.redirect("/lego/sets");
+  } catch (err) {
+    res.render("500", {
+      message: `I'm sorry, but we have encountered the following error: ${err}`,
     });
   }
 });
